51650f094cf6e70818add80d0e8228351576f0ca
Cooper Knaak <cooperknaak@gmail.com>
2016-09-04 22:51:24 -0500

    Initial Commit

---
+++ CoronaConvenience/CoronaConvenience.h
@@ -0,0 +1,19 @@
+//
+//  CoronaConvenience.h
+//  CoronaConvenience
+//
+//  Created by Cooper Knaak on 9/4/16.
+//  Copyright © 2016 Cooper Knaak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoronaConvenience.
+FOUNDATION_EXPORT double CoronaConvenienceVersionNumber;
+
+//! Project version string for CoronaConvenience.
+FOUNDATION_EXPORT const unsigned char CoronaConvenienceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoronaConvenience/PublicHeader.h>
+
+
