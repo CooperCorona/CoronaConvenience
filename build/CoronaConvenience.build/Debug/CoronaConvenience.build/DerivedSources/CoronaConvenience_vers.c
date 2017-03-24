082973797fcb909f4a1036acda1ebff61963d857
Cooper Knaak <cooperknaak@gmail.com>
2017-03-23 19:47:22 -0500

    Removed cached files in build directory (which were added before updating the .gitignore).

--- build/CoronaConvenience.build/Debug/CoronaConvenience.build/DerivedSources/CoronaConvenience_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char CoronaConvenienceVersionString[];
- extern const double CoronaConvenienceVersionNumber;
-
- const unsigned char CoronaConvenienceVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CoronaConvenience  PROJECT:CoronaConvenience-1" "\n";
- const double CoronaConvenienceVersionNumber __attribute__ ((used)) = (double)1.;
